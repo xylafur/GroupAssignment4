c76bfacba8c067ab9a2fd272f052869b4b50becc
VisualizerMk2 <aaronsaccts@gmail.com>
2017-04-05 00:32:43 -0500

    Update sortedArray.h
    added high res timer for insert, delete, search

--- ADTs/sortedArray.h
+++ ADTs/sortedArray.h
@@ -1,3 +1,10 @@
+//
+// Created by Aaron on 4/4/2017.
+//
+
+#ifndef GROUPASSIGN4_SORTEDARRAY_H
+#define GROUPASSIGN4_SORTEDARRAY_H
+
 #ifndef SORTED_ARRAY
 #define SORTED_ARRAY
 #include <stdio.h>
@@ -37,7 +44,7 @@ public:
     }
     void insert(T val){
         auto start = high_resolution_clock::now();
-        
+
         if(realSize >= size)
             std::cout<<"trying to put to many elements in your sorted array..\n";
         else{
@@ -57,11 +64,11 @@ public:
         auto stop = high_resolution_clock::now();
 
         std::cout << "Time to insert: " << duration_cast<milliseconds>(stop - start).count() << " milliseconds" << std::endl;
-        
+
     }//end insert
     void deleteAtIndex(int index){
         auto start = high_resolution_clock::now();
-        
+
         if( index > realSize )
             std::cout<<"Invalid index to delete bruh\n";
         else{
@@ -111,12 +118,12 @@ public:
     }
     int search(T val){
         auto start = high_resolution_clock::now();
-        
+
         std::cout<<"calling search helper with the values hi:"<<realSize<<std::endl;
         //return searchHelper(val, realSize, 0);
 
         auto stop = high_resolution_clock::now();
-        
+
         std::cout << "Time to search: " << duration_cast<milliseconds>(stop - start).count() << " milliseconds" << std::endl;
         return iterativeBinSearch(val);
     }
@@ -130,3 +137,6 @@ public:
 
 
 #endif
+
+
+#endif //GROUPASSIGN4_SORTEDARRAY_H
